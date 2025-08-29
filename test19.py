8745e4be05458b20367b909722a5a5251fa3cd74
Danirut Muangjan <danirut9986@gmail.com>
2025-08-29 04:30:01 +0000

    finale

---
+++ test19.py
@@ -0,0 +1,22 @@
+print('--------------------------')
+print('calculate_commission')
+print('--------------------------')
+
+emp_id = input("กรุณาป้อนรหัสพนักงาน: ")
+emp_name = input("กรุณาป้อนชื่อพนักงาน: ")
+sales = float(input("กรุณาป้อนยอดขาย (บาท): "))
+
+
+if sales <= 1000:
+    commission = 0.0
+elif 1001 <= sales <= 2000:
+    commission = sales * 0.01
+elif 2001 <= sales <= 3000:
+    commission = sales * 0.03
+else:
+    commission = sales * 0.05
+
+print(f"รหัสพนักงาน: {emp_id}")
+print(f"ชื่อพนักงาน: {emp_name}")
+print(f"ยอดขาย: {sales} บาท")
+print(f"ค่าคอมมิชชั่น: {commission} บาท")
